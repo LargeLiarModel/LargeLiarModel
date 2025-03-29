0ad811a2d97e95e8a288e891ae0f18e66a26ee0e
Matthew Mullori <smm200408@gmail.com>
2025-03-29 17:53:35 -0400

    added combined primary keys to schema

--- apps/hono-api/src/drizzle/schema.ts
+++ apps/hono-api/src/drizzle/schema.ts
@@ -1,4 +1,10 @@
-import { integer, real, text, sqliteTable } from "drizzle-orm/sqlite-core";
+import {
+  integer,
+  real,
+  text,
+  sqliteTable,
+  primaryKey,
+} from "drizzle-orm/sqlite-core";
 
 export const Candidate = sqliteTable("Candidate", {
   CID: text().primaryKey(),
@@ -6,19 +12,23 @@ export const Candidate = sqliteTable("Candidate", {
 });
 
 // https://www.opensecrets.org/resources/datadictionary/Data%20Dictionary%20Candidates%20Data.htm
-export const Candidacies = sqliteTable("Candidacies", {
-  Cycle: integer(),
-  FECCandID: text().primaryKey(),
-  CID: text().references(() => Candidate.CID),
-  Party: text(),
-  DistIDRunFor: text(),
-  DistIDCurr: text(),
-  CurrCand: text(),
-  CycleCand: text(),
-  CRPICO: text(),
-  RecipCode: text(),
-  NoPacs: text(),
-});
+export const Candidacies = sqliteTable(
+  "Candidacies",
+  {
+    Cycle: integer(),
+    FECCandID: text(),
+    CID: text().references(() => Candidate.CID, { onDelete: "cascade" }),
+    Party: text(),
+    DistIDRunFor: text(),
+    DistIDCurr: text(),
+    CurrCand: text(),
+    CycleCand: text(),
+    CRPICO: text(),
+    RecipCode: text(),
+    NoPacs: text(),
+  },
+  (table) => [primaryKey({ columns: [table.Cycle, table.FECCandID] })]
+);
 
 // https://www.opensecrets.org/resources/datadictionary/Data%20Dictionary%20for%20Cmtes.htm
 export const Committees = sqliteTable("Committees", {
@@ -39,42 +49,52 @@ export const Committees = sqliteTable("Committees", {
 });
 
 // https://www.opensecrets.org/resources/datadictionary/Data%20Dictionary%20for%20PAC%20to%20Cands%20Data.htm
-export const PAC_Candidate = sqliteTable("PAC_Candidate", {
-  Cycle: integer(),
-  FECRecNo: text().primaryKey(),
-  PACID: text().references(() => Committees.CmteID),
-  CID: text().references(() => Candidate.CID),
-  Amount: real(),
-  Date: text(),
-  RealCode: text(),
-  Type: text(),
-  DI: text(),
-  FECCandID: text().references(() => Candidacies.FECCandID),
-});
+export const PAC_Candidate = sqliteTable(
+  "PAC_Candidate",
+  {
+    Cycle: integer(),
+    FECRecNo: text().primaryKey(),
+    PACID: text().references(() => Committees.CmteID, { onDelete: "cascade" }),
+    CID: text().references(() => Candidate.CID, { onDelete: "cascade" }),
+    Amount: real(),
+    Date: text(),
+    RealCode: text(),
+    Type: text(),
+    DI: text(),
+    FECCandID: text().references(() => Candidacies.FECCandID, {
+      onDelete: "cascade",
+    }),
+  },
+  (table) => [primaryKey({ columns: [table.Cycle, table.FECRecNo] })]
+);
 
 // https://www.opensecrets.org/resources/datadictionary/Data%20Dictionary%20PAC%20to%20PAC%20Data.htm
-export const PAC_PAC = sqliteTable("PAC_PAC", {
-  Cycle: integer(),
-  FECRecNo: text().primaryKey(),
-  FilerID: text(),
-  DonorCmte: text(),
-  ContribLendTrans: text(),
-  City: text(),
-  State: text(),
-  Zip: text(),
-  FECOccEmp: text(),
-  PrimCode: text(),
-  Date: text(),
-  Amount: real(),
-  RecipID: text(),
-  Party: text(),
-  Otherid: text(),
-  RecipCode: text(),
-  RecipPrimcode: text(),
-  Amend: text(),
-  Report: text(),
-  Microfilm: text(),
-  Type: text(),
-  RealCode: text(),
-  Source: text(),
-});
+export const PAC_PAC = sqliteTable(
+  "PAC_PAC",
+  {
+    Cycle: integer(),
+    FECRecNo: text().primaryKey(),
+    FilerID: text(),
+    DonorCmte: text(),
+    ContribLendTrans: text(),
+    City: text(),
+    State: text(),
+    Zip: text(),
+    FECOccEmp: text(),
+    PrimCode: text(),
+    Date: text(),
+    Amount: real(),
+    RecipID: text(),
+    Party: text(),
+    Otherid: text(),
+    RecipCode: text(),
+    RecipPrimcode: text(),
+    Amend: text(),
+    Report: text(),
+    Microfilm: text(),
+    Type: text(),
+    RealCode: text(),
+    Source: text(),
+  },
+  (table) => [primaryKey({ columns: [table.Cycle, table.FECRecNo] })]
+);
