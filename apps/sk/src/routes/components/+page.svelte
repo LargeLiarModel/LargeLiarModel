0290fcdeb150763f221696d9a4fc83715562d2b4
Rushil Umaretiya <rushilwiz@gmail.com>
2025-03-29 17:53:32 -0400

    chore: working on components

---
+++ apps/sk/src/routes/components/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import Button from "$lib/components/ui/button/button.svelte";
+import { MoveRight } from "@lucide/svelte";
+</script>
+
+<div class="">
+  <div class="h-[80vh] w-[80vw] bg-white">
+      <h1 class="">Question 1</h1>
+      <p>What is the capital of France?</p>
+      
+    </div>
+</div>
