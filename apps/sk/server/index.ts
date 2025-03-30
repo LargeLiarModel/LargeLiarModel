49291df2c61a9930a07db82b433c56aab1b3b09b
Steven Stavrakis <s.a.stavrakis@gmail.com>
2025-03-30 11:59:31 -0400

    h

--- apps/sk/server/index.ts
+++ apps/sk/server/index.ts
@@ -1,2 +1,6 @@
+import app from "./app";
 export { hcWithType } from "./client";
 export type { AppType } from "./app";
+
+// Export the app as the default export for Vercel to use
+export default app;
